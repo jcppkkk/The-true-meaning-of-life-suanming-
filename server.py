59f6dce5f6d7f45f577252e7780a44f586e8c264
Jethro Yu <comet.jc@gmail.com>
2020-10-14 12:32:05 +0800

    Convert to zh_tw.utf-8, black format

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 from sanic import Sanic, response
 from sanic.response import text
 
@@ -6,26 +6,28 @@ from get_life import YEAR_MAP, MONTH_MAP, DAY_MAP, HOUR, LIFE_MAP
 
 app = Sanic(__name__)
 
+
 @app.route("/")
 async def home(request):
-    return text('称骨算命法，相传是唐朝周易大师袁天罡先生所创，其法将人的生辰八字，即出生的农历年月日时计算相应的“骨重”，然后根据“称骨”的总值来进行算命。(古代的重量单位：1斤=10两，1两=10钱)')
+    return text(
+        "稱骨算命法，相傳是唐朝週易大師袁天罡先生所創，其法將人的生辰八字，即出生的農歷年月日時計算相應的“骨重”，然後根據“稱骨”的總值來進行算命。(古代的重量單位：1斤=10兩，1兩=10錢)"
+    )
 
 
-@app.route("/suan_ming",methods=["POST"])
+@app.route("/suan_ming", methods=["POST"])
 async def post_data(request):
-  year = request.args.get("year")
-  month = request.args.get("month")
-  day = request.args.get("day")
-  hour = request.args.get("hour")
-  try:
-      score = YEAR_MAP[year] + MONTH_MAP[month] + DAY_MAP[day] + HOUR[hour]
-      life_language = LIFE_MAP[str(int(score))]
-      weight_list = str(score).split(".")
-      life_weight = weight_list[0] + "两" + weight_list[1] + "钱"
-  except:
-      life_language,life_weight ="我命由我不由天！","0"
-  return response.json(
-        {'life_language': life_language,
-         'life_weight':life_weight})
+    year = request.args.get("year")
+    month = request.args.get("month")
+    day = request.args.get("day")
+    hour = request.args.get("hour")
+    try:
+        score = YEAR_MAP[year] + MONTH_MAP[month] + DAY_MAP[day] + HOUR[hour]
+        life_language = LIFE_MAP[str(int(score))]
+        weight_list = str(score).split(".")
+        life_weight = weight_list[0] + "兩" + weight_list[1] + "錢"
+    except:
+        life_language, life_weight = "我命由我不由天！", "0"
+    return response.json({"life_language": life_language, "life_weight": life_weight})
+
 
 app.run(host="0.0.0.0", port=8000, debug=True)
